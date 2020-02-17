89037295c16fa72903611ef48db496e3fc5fe943
Colin Walker <me@colinjfw.com>
2020-02-17 08:48:38 -0800

    Configure dependencies

--- example/config.jbld.js
+++ example/config.jbld.js
@@ -2,6 +2,10 @@ const { BabelPlugin } = require("../lib/babel");
 const { Configuration } = require("../lib");
 
 const babel = new BabelPlugin({
+  plugins: [
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-syntax-dynamic-import",
+  ],
   presets: [
     "minify",
     [
